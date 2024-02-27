e09e7349f704060da4b84d40650d37798bdcc8e0
nowadraco <nowadraco@gmail.com>
2024-02-27 16:18:14 -0300

    modificando js do menu

--- src/js/menu-hamburger.js
+++ src/js/menu-hamburger.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuItems = document.querySelector('.menu-itens');
-  
-    menuToggle.addEventListener('click', function() {
-      // Alternar a classe 'active' nos itens do menu
-      menuItems.classList.toggle('active');
-  
-      // Alternar entre ícone de hamburger e ícone de "x"
-      menuToggle.querySelector('.hamburger').classList.toggle('close');
-    });
-  });
+document.querySelector('men-toggle').addEvenrListener('click', function() {
+  document.querySelector('menu').classList.toggle('active')
+});
